49e634badcc25d995bc34152080af44b6c4b7ccb
a114221036-cpu <a114221036@mail.shu.edu.tw>
2025-10-29 13:00:35 +0800

    114_10_29

---
+++ 114_10_29/loginDemo/loginDemo/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace loginDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        // 新增的控制項欄位
+        private System.Windows.Forms.Label labelAccount;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxAccount;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.Label labelResult;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelAccount = new System.Windows.Forms.Label();
+            this.textBoxAccount = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelAccount
+            // 
+            this.labelAccount.AutoSize = true;
+            this.labelAccount.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.labelAccount.Location = new System.Drawing.Point(50, 50);
+            this.labelAccount.Name = "labelAccount";
+            this.labelAccount.Size = new System.Drawing.Size(156, 55);
+            this.labelAccount.TabIndex = 0;
+            this.labelAccount.Text = "帳號：";
+            // 
+            // textBoxAccount
+            // 
+            this.textBoxAccount.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.textBoxAccount.Location = new System.Drawing.Point(200, 47);
+            this.textBoxAccount.Name = "textBoxAccount";
+            this.textBoxAccount.Size = new System.Drawing.Size(450, 66);
+            this.textBoxAccount.TabIndex = 1;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.labelPassword.Location = new System.Drawing.Point(50, 130);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(156, 55);
+            this.labelPassword.TabIndex = 2;
+            this.labelPassword.Text = "密碼：";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.textBoxPassword.Location = new System.Drawing.Point(200, 127);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(450, 66);
+            this.textBoxPassword.TabIndex = 3;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.buttonCheck.Location = new System.Drawing.Point(267, 215);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(150, 60);
+            this.buttonCheck.TabIndex = 4;
+            this.buttonCheck.Text = "檢查";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelResult.Font = new System.Drawing.Font("微軟正黑體", 22F);
+            this.labelResult.Location = new System.Drawing.Point(50, 300);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(600, 180);
+            this.labelResult.TabIndex = 5;
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 573);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonCheck);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBoxAccount);
+            this.Controls.Add(this.labelAccount);
+            this.Name = "Form1";
+            this.Text = "登入範例";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+
