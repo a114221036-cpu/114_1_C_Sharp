9c9eb3b38d225ac5289ff282d5e062df5377b99f
a114221036-cpu <a114221036@mail.shu.edu.tw>
2025-10-29 12:29:48 +0800

    114_10_29

---
+++ 114_10_29/MultipleConditionsDemo/MultipleConditionsDemo/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace MultipleConditionsDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.Button judgeButton;
+        private System.Windows.Forms.Label gradeLabel;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.judgeButton = new System.Windows.Forms.Button();
+            this.gradeLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("新細明體", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblScore.Location = new System.Drawing.Point(107, 142);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(154, 44);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "成績：";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("新細明體", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtScore.Location = new System.Drawing.Point(267, 126);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(227, 60);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.TextChanged += new System.EventHandler(this.txtScore_TextChanged);
+            // 
+            // judgeButton
+            // 
+            this.judgeButton.Font = new System.Drawing.Font("新細明體", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.judgeButton.Location = new System.Drawing.Point(115, 332);
+            this.judgeButton.Name = "judgeButton";
+            this.judgeButton.Size = new System.Drawing.Size(304, 106);
+            this.judgeButton.TabIndex = 2;
+            this.judgeButton.Text = "等級判斷";
+            this.judgeButton.UseVisualStyleBackColor = true;
+            this.judgeButton.Click += new System.EventHandler(this.btnJudge_Click);
+            // 
+            // gradeLabel
+            // 
+            this.gradeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.gradeLabel.Font = new System.Drawing.Font("新細明體", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.gradeLabel.Location = new System.Drawing.Point(267, 225);
+            this.gradeLabel.Name = "gradeLabel";
+            this.gradeLabel.Size = new System.Drawing.Size(242, 60);
+            this.gradeLabel.TabIndex = 3;
+            this.gradeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.gradeLabel.Click += new System.EventHandler(this.gradeLabel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(107, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 44);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "成績：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 903);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gradeLabel);
+            this.Controls.Add(this.judgeButton);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 114_10_29/MultipleConditionsDemo/MultipleConditionsDemo/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultipleConditionsDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJudge_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int score = int.Parse(txtScore.Text);
+                string grade;
+                if (score >= 90)
+                {
+                    grade = "A";
+                }
+                else if (score >= 80) // score < 90 是隱含條件
+                {
+                    grade = "B";
+                }
+                else if (score >= 70) // score < 80 是隱含條件
+                {
+                    grade = "C";
+                }
+                else if (score >= 60) // score < 70 是隱含條件
+                {
+                    grade = "D";
+                }
+                else
+                {
+                    grade = "F";
+
+                }
+                if (score >= 60)
+                {
+                    if (score >= 70)
+                    {
+                        if (score >= 80)
+                        {
+                            if (score >= 90)
+                            {
+                                grade = "A";
+                            }
+                            else
+                            {
+                                grade = "B";
+                            }
+                        }
+                        else
+                        {
+                            grade = "C";
+                        }
+                    }
+                    else
+                    {
+                        grade = "D";
+                    }
+                }
+
+                else
+                {
+                    grade = "F";
+                }
+
+
+
+                 gradeLabel.Text = grade;
+           
+            
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("請輸入有效的數字成績。", "輸入錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void gradeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtScore_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
