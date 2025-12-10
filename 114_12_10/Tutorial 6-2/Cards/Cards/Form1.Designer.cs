889ceec5bf272d7b7845d2640c4218d49b8713ff
a114221036-cpu <a114221036@mail.shu.edu.tw>
2025-12-10 11:37:32 +0800

    114_12_10

---
+++ 114_12_10/Tutorial 6-2/Cards/Cards/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Cards
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">若為 true 則釋放受控資源，反之僅釋放非受控資源。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// 初始化元件。已將所有可視元件的文字改為繁體中文、字型大小統一為 18，並依字型大小適度調整元件大小與相對位置。
+        /// 重要：僅變更文字、字型、大小與位置，其他行為（事件綁定、資源圖片名稱等）維持不變。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cardListBox = new System.Windows.Forms.ListBox();
+            this.showCardButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.cardPanel = new System.Windows.Forms.Panel();
+            this.kingClubsPictureBox = new System.Windows.Forms.PictureBox();
+            this.tenHeartsPictureBox = new System.Windows.Forms.PictureBox();
+            this.aceSpadesPictureBox = new System.Windows.Forms.PictureBox();
+            this.cardPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kingClubsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tenHeartsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aceSpadesPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cardListBox
+            // 
+            this.cardListBox.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cardListBox.FormattingEnabled = true;
+            this.cardListBox.ItemHeight = 45;
+            this.cardListBox.Items.AddRange(new object[] {
+            "黑桃 A",
+            "紅心 10",
+            "梅花 K"});
+            this.cardListBox.Location = new System.Drawing.Point(303, 416);
+            this.cardListBox.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.cardListBox.Name = "cardListBox";
+            this.cardListBox.Size = new System.Drawing.Size(473, 184);
+            this.cardListBox.TabIndex = 3;
+            this.cardListBox.SelectedIndexChanged += new System.EventHandler(this.cardListBox_SelectedIndexChanged);
+            // 
+            // showCardButton
+            // 
+            this.showCardButton.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.showCardButton.Location = new System.Drawing.Point(120, 643);
+            this.showCardButton.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.showCardButton.Name = "showCardButton";
+            this.showCardButton.Size = new System.Drawing.Size(334, 132);
+            this.showCardButton.TabIndex = 4;
+            this.showCardButton.Text = "顯示牌面";
+            this.showCardButton.UseVisualStyleBackColor = true;
+            this.showCardButton.Click += new System.EventHandler(this.showCardButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.exitButton.Location = new System.Drawing.Point(583, 643);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(334, 132);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "離開";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // cardPanel
+            // 
+            this.cardPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.cardPanel.Controls.Add(this.kingClubsPictureBox);
+            this.cardPanel.Controls.Add(this.tenHeartsPictureBox);
+            this.cardPanel.Controls.Add(this.aceSpadesPictureBox);
+            this.cardPanel.Location = new System.Drawing.Point(42, 42);
+            this.cardPanel.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.cardPanel.Name = "cardPanel";
+            this.cardPanel.Size = new System.Drawing.Size(992, 336);
+            this.cardPanel.TabIndex = 7;
+            // 
+            // kingClubsPictureBox
+            // 
+            this.kingClubsPictureBox.Image = global::Cards.Properties.Resources.King_Clubs;
+            this.kingClubsPictureBox.Location = new System.Drawing.Point(618, 35);
+            this.kingClubsPictureBox.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.kingClubsPictureBox.Name = "kingClubsPictureBox";
+            this.kingClubsPictureBox.Size = new System.Drawing.Size(291, 267);
+            this.kingClubsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.kingClubsPictureBox.TabIndex = 5;
+            this.kingClubsPictureBox.TabStop = false;
+            this.kingClubsPictureBox.Visible = false;
+            // 
+            // tenHeartsPictureBox
+            // 
+            this.tenHeartsPictureBox.Image = global::Cards.Properties.Resources._10_Hearts;
+            this.tenHeartsPictureBox.Location = new System.Drawing.Point(353, 35);
+            this.tenHeartsPictureBox.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.tenHeartsPictureBox.Name = "tenHeartsPictureBox";
+            this.tenHeartsPictureBox.Size = new System.Drawing.Size(213, 267);
+            this.tenHeartsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tenHeartsPictureBox.TabIndex = 4;
+            this.tenHeartsPictureBox.TabStop = false;
+            this.tenHeartsPictureBox.Visible = false;
+            // 
+            // aceSpadesPictureBox
+            // 
+            this.aceSpadesPictureBox.Image = global::Cards.Properties.Resources.Ace_Spades;
+            this.aceSpadesPictureBox.Location = new System.Drawing.Point(35, 35);
+            this.aceSpadesPictureBox.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.aceSpadesPictureBox.Name = "aceSpadesPictureBox";
+            this.aceSpadesPictureBox.Size = new System.Drawing.Size(256, 267);
+            this.aceSpadesPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.aceSpadesPictureBox.TabIndex = 3;
+            this.aceSpadesPictureBox.TabStop = false;
+            this.aceSpadesPictureBox.Visible = false;
+            this.aceSpadesPictureBox.Click += new System.EventHandler(this.aceSpadesPictureBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(21F, 45F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1049, 911);
+            this.Controls.Add(this.cardPanel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.showCardButton);
+            this.Controls.Add(this.cardListBox);
+            this.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.Name = "Form1";
+            this.Text = "撲克牌";
+            this.cardPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kingClubsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tenHeartsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aceSpadesPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox cardListBox;
+        private System.Windows.Forms.Button showCardButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Panel cardPanel;
+        private System.Windows.Forms.PictureBox kingClubsPictureBox;
+        private System.Windows.Forms.PictureBox tenHeartsPictureBox;
+        private System.Windows.Forms.PictureBox aceSpadesPictureBox;
+    }
+}
+
---
+++ 114_12_10/Tutorial 6-2/Cards/Cards/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cards
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showCardButton_Click(object sender, EventArgs e)
+        {
+         if (cardListBox.SelectedIndex != -1)
+         {
+             showSelectedCard(cardListBox.SelectedItem.ToString());
+            }
+         else
+         {
+             MessageBox.Show("請先選擇一張牌。");
+            }
+        }
+
+        private void showSelectedCard(string cardName)
+        {
+           switch(cardName) 
+            {
+                case "黑桃 A":
+                    // Add code to show Ace of Spades
+                    showAceSpads(); 
+                    break;
+                case "紅心 10":
+                    // Add code to show Ten of Hearts 
+                    showTenHearts();
+                    break;
+                case "梅花 K":
+                    // Add code to show King of Clubs
+                    showKingClubs();    
+                    break;
+                default:
+                    // Add code to handle unexpected input
+                    break;
+            }   
+        }
+
+        private void showAceSpads()
+        {
+            aceSpadesPictureBox.Visible = true;
+            tenHeartsPictureBox.Visible = false;
+            kingClubsPictureBox.Visible = false;
+        }   
+
+        private void showTenHearts()
+        {
+            aceSpadesPictureBox.Visible = false;
+            tenHeartsPictureBox.Visible = true;
+            kingClubsPictureBox.Visible = false;
+        }
+
+
+        private void showKingClubs()
+        {
+            aceSpadesPictureBox.Visible = false;
+            tenHeartsPictureBox.Visible = false;
+            kingClubsPictureBox.Visible = true;
+        }   
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+
+        private void cardListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aceSpadesPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
